3e40c55db0ddaf9201054248ed23301530d20f25
Matheus Seghatti <matheusseghatti@gmail.com>
2025-11-14 16:50:12 -0300

    dei uma pensada melhor nas coisas

--- ag.cpp
+++ ag.cpp
@@ -9,13 +9,35 @@ using namespace std;
     "Os paraˆmetros empregados ao longo da execuc ̧ a ̃o devem ser definidos na interface do 
     sistema: taxas de cruzamento e mutac ̧ a ̃o, tamanho da populac ̧ a ̃o e nu ́ mero de gerac ̧o ̃es";
 
+    //tá, acho que entendi. A gente precisa definir a taxa de cruzamento e mutação, o tamanho da população e o número de gerações, né?
+
     nmrl q a gnt vai ter q operar com float?
     eu não faço ideia de como q faz os gráfico e os crl; se n me engano o b-word falo 
     q ia disponibilziar um exemplo mas até agora nada
 */
 
-class Individuo {};
-class Populacao {};
+struct Individuo { 
+    unsigned gen_x, gen_y;
+    unsigned fitness; //z
+    Individuo* next;
+
+    Individuo(float x, float y): gen_x(x), gen_y(y), fitness(905), next(nullptr) {}
+};
+
+class Populacao { //não sei direito como q vamos interagir com as gerações e os crl
+public:
+    Individuo* base;
+    Populacao(){
+        Individuo b(-15, -15);
+        base = &b;
+    }
+
+    unsigned FloaToBinary(float f) {}   //armazenar em cada indivíduo       -> dá pra chamar a função de Code
+    unsigned BinaryToFloat(unsigned) {} //usar para a função de otimização  -> dá pra chamar a função de Decode
+};
+
+class Geracoes {}; //chapei demais?
+
 
 Individuo Select(Populacao p) {} //função de seleção (por ranking)
 
@@ -26,11 +48,8 @@ Individuo Mutacao(Individuo x) {} //função de mutação (inversão dos bits)
 /*elitismo acho que não precisa função, só pegar o indivíduo retornado da função de seleção
 e passar pra proxima geração (posso estar errado)*/
 
-float Otimizacao(int x, int y) { //função de otimização -> ela recebe int mesmo? ela retorna o fitness né?
-    //não entendi as restrições da função (x ∈[−15,15], y ∈[−15,15])
+float Otimizacao(float x, float y) {
     //tb n entendi qual é a dos valores base
 
-    return (2*(x*x) - 13*x + x*y - (7*y)/3); //provavelmente não é simples assim. e não sei se é 7y/3 ou 7^(y/3)
+    return (2*(x*x) - 13*x + x*y - (7*y)/3);
 } 
-
-//acho que precisa de uma função pra codificação também, mas não tenho ctz de como deveria ser feita
